c389cc94bc8cd18148fe059148c2bc25494911a6
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-06-24 23:18:58 +0900

    How do I build custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,56 +6,24 @@ class DemoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new Scaffold(body: new Signature()),
+      home: new Scaffold(
+          body: new Center(
+        child: new CustomButton("Hello"),
+      )),
     );
   }
 }
 
-class Signature extends StatefulWidget {
-  @override
-  _SignatureState createState() => new _SignatureState();
-}
+class CustomButton extends StatelessWidget {
+  final String label;
 
-class _SignatureState extends State<Signature> {
-  List<Offset> _points = [];
+  CustomButton(this.label);
 
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
-      onPanUpdate: (DragUpdateDetails details) {
-        setState(() {
-          RenderBox referenceBox = context.findRenderObject();
-          Offset localPosition =
-              referenceBox.globalToLocal(details.globalPosition);
-          _points = new List.from(_points)..add(localPosition);
-        });
-      },
-      onPanEnd: (DragEndDetails details) => _points.add(null),
-      child: new CustomPaint(
-          painter: new SignaturePainter(_points), size: Size.infinite),
+    return new RaisedButton(
+      onPressed: () {},
+      child: new Text(label),
     );
-  }
-}
-
-class SignaturePainter extends CustomPainter {
-  SignaturePainter(this.points);
-
-  final List<Offset> points;
-
-  void paint(Canvas canvas, Size size) {
-    Paint paint = new Paint()
-      ..color = Colors.black
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = 5.0;
-    for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] != null) {
-        canvas.drawLine(points[i], points[i + 1], paint);
-      }
-    }
-  }
-
-  @override
-  bool shouldRepaint(SignaturePainter oldDelegate) {
-    return oldDelegate.points != this.points;
   }
 }
