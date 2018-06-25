85b87378694651f1bab65cb5d7ab479920a59dc4
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-06-25 10:07:24 +0900

    画面遷移:各ページに行って戻るようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new DemoApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new MyAppHome(),
+    routes: <String, WidgetBuilder>{
+      '/a': (BuildContext context) => new MyPage(title: 'Page A'),
+      '/b': (BuildContext context) => new MyPage(title: 'Page B'),
+      '/c': (BuildContext context) => new MyPage(title: 'Page C'),
+    },
+  ));
+}
 
-class DemoApp extends StatelessWidget {
+class MyPage extends StatelessWidget {
+  final String title;
+
+  MyPage({this.title});
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-          body: new Center(
-        child: new CustomButton("Hello"),
-      )),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(this.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new RaisedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: new Text('go back'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class CustomButton extends StatelessWidget {
-  final String label;
+class MyAppHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Sample App"),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new ButtonWidget('Go to Page A', '/a'),
+            new ButtonWidget('Go to Page B', '/b'),
+            new ButtonWidget('Go to Page C', '/c'),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-  CustomButton(this.label);
+class ButtonWidget extends StatelessWidget {
+  final String title;
+  final String destination;
+
+  ButtonWidget(this.title, this.destination);
 
   @override
   Widget build(BuildContext context) {
-    return new RaisedButton(
-      onPressed: () {},
-      child: new Text(label),
+    return new Container(
+      padding: EdgeInsets.all(8.0),
+      child: new RaisedButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed(destination);
+        },
+        child: new Text(title),
+      ),
     );
   }
 }
