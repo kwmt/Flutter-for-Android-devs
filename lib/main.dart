da43f06a9dc3d6b421fab97923867fb77a26ba09
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-06-26 23:46:42 +0900

    row,columnのサンプル

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,56 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new Center(child: new LifecycleWatcher()));
+  runApp(new LinearLayoutSample());
 }
 
-class LifecycleWatcher extends StatefulWidget {
-  @override
-  _LifecycleWatcherState createState() => new _LifecycleWatcherState();
-}
-
-class _LifecycleWatcherState extends State<LifecycleWatcher>
-    with WidgetsBindingObserver {
-  AppLifecycleState _lastLifecycleState;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    setState(() {
-      print(state);
-      _lastLifecycleState = state;
-    });
-  }
-
+class LinearLayoutSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    if (_lastLifecycleState == null) {
-      return new Text('This widget has not observed any lifecycle changes.',
-          textDirection: TextDirection.ltr);
-    }
-    return new Text(
-        'The most recent lifecycle state this widget observed was: $_lastLifecycleState.',
-        textDirection: TextDirection.ltr);
+    return new MaterialApp(
+        title: 'LinearLayoutSample',
+        home: new Scaffold(
+          body: new Center(
+            child: new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                new Text(
+                  "Row Sample",
+                  style: new TextStyle(
+                      fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                new Container(
+                    padding: EdgeInsets.only(bottom: 32.0),
+                    // 以下がRowのサンプル
+                    child: new Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Text('Row One,'),
+                        new Text('Row Two,'),
+                        new Text('Row Three,'),
+                        new Text('Row Four'),
+                      ],
+                    )),
+                new Text(
+                  "Column Sample",
+                  style: new TextStyle(
+                      fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                // 以下がColumnのサンプル
+                new Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Text('Column One'),
+                    new Text('Column Two'),
+                    new Text('Column Three'),
+                    new Text('Column Four'),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
