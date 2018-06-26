0d416585aaa49fda98bc21fe47786133d5f0bc05
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-06-26 16:06:54 +0900

    use http

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'dart:convert';
-import 'dart:isolate';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -67,36 +65,12 @@ class _SampleAppPageState extends State<SampleAppPage> {
   }
 
   loadData() async {
-    ReceivePort receivePort = new ReceivePort();
-    await Isolate.spawn(dataLoader, receivePort.sendPort);
-
-    SendPort sendPort = await receivePort.first;
-    List msg = await sendReceive(
-        sendPort, "https://jsonplaceholder.typicode.com/posts");
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(dataURL);
 
     setState(() {
-      widgets = msg;
+      widgets = json.decode(response.body);
     });
-  }
-
-  static dataLoader(SendPort sendPort) async {
-    ReceivePort port = new ReceivePort();
-    sendPort.send(port.sendPort);
-
-    await for (var msg in port) {
-      String data = msg[0];
-      SendPort replyTo = msg[1];
-
-      String dataURL = data;
-      http.Response response = await http.get(dataURL);
-      replyTo.send(json.decode(response.body));
-    }
-  }
-
-  Future sendReceive(SendPort port, msg) {
-    ReceivePort response = new ReceivePort();
-    port.send([msg, response.sendPort]);
-    return response.first;
   }
 
   getProgressDialog() {
