86313376eaf09f1474d32f610380318256a3306f
Yasutaka Kawamoto <kwmt27@gmail.com>
2018-06-27 00:16:51 +0900

    scrollview sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new LinearLayoutSample());
+  runApp(new ScrollViewSample());
 }
 
-class LinearLayoutSample extends StatelessWidget {
+class ScrollViewSample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'LinearLayoutSample',
+        title: 'ScrollViewSample',
         home: new Scaffold(
-          body: new Center(
-            child: new Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                new Text(
-                  "Row Sample",
-                  style: new TextStyle(
-                      fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                new Container(
-                    padding: EdgeInsets.only(bottom: 32.0),
-                    // 以下がRowのサンプル
-                    child: new Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        new Text('Row One,'),
-                        new Text('Row Two,'),
-                        new Text('Row Three,'),
-                        new Text('Row Four'),
-                      ],
-                    )),
-                new Text(
-                  "Column Sample",
-                  style: new TextStyle(
-                      fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                // 以下がColumnのサンプル
-                new Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Text('Column One'),
-                    new Text('Column Two'),
-                    new Text('Column Three'),
-                    new Text('Column Four'),
-                  ],
-                )
-              ],
-            ),
+          body: new ListView(
+            children: createSampleList(),
           ),
         ));
+  }
+
+  createSampleList() {
+    List<Widget> list = new List();
+    for (var i = 0; i < 100; i++) {
+      list.add(new Text('Row $i'));
+    }
+    return list;
   }
 }
